9acd45abd2e115873500fe730d3f359aee4e4b5b
DESKTOP-JLN1BD1\jonag <jonaglon@gmail.com>
2019-11-07 20:39:52 +0000

    RGBW stripes in all directions working nicely and in time.

--- Ravebot_LightsBrain/doPatternStripes.ino
+++ Ravebot_LightsBrain/doPatternStripes.ino
@@ -1,15 +1,15 @@
 
 void doPatternStripes() {
   if (currentLightPattern == 14) {
-    int currentStripePattern = (timeyInTime/524288)%2;
-    stripesPattern(currentStripePattern+2);
+    int currentStripePattern = (timeyInTime/524288)%4;
+    stripesPattern(currentStripePattern);
   } else if (currentLightPattern == 15) {
     stripesPattern(1);
   }  
 }
 
 // JR TODO: ...
-//  1 Get good timings for the 1-2-3-4 for each of the four directions
+//  x 1 Get good timings for the 1-2-3-4 for each of the four directions
 //  2 Wipes, on the beat l-r, top-bot, r-l, bot-top, repeat. Use good colours.
 //  3 KinghtRider with big-ish LUT?
 //  The stripe based rainbows probly belong here.
@@ -32,37 +32,32 @@ struct stripies {
 };
 
 // Stripe patterns - 
-// 1 - Random good colors moving slowly top to bottom
+// 1 - Random good colors moving slowly top to bottom - TODO next, consider if lots of little array patterns like below are best way to handle stipes.
 // 2 - 
-const byte numStripesPattern1 = 4;
-stripies stripePatterns[4][4] = {
-{ {0, 0, 0, 0, 1, 256, 512, 32, true, true },
-  {0, 0, 0, 0, 2, 256, 1536, 32, true, true }, 
-  {0, 0, 0, 0, 3, 256, 2560, 32, true, true },
-  {0, 0, 0, 0, 4, 256, 3584, 32, true, true } },
-{ {0, 0, 0, 0, 1, 256, 1536, 32, false, false },
-  {0, 0, 0, 0, 2, 256, 512, 32, false, false },
-  {0, 0, 0, 0, 3, 256, 3584, 32, false, false },
-  {0, 0, 0, 0, 4, 256, 2560, 32, false, false } },
-{ {0, 0, 0, 0, 1, 256, 2560, 32, true, false },
-  {0, 0, 0, 0, 2, 256, 3584, 32, true, false },
-  {0, 0, 0, 0, 3, 256, 512, 32, true, false },
-  {0, 0, 0, 0, 4, 256, 1536, 32, true, false } },
-{ {0, 0, 0, 0, 1, 256, 2304, 32, false, true },
-  {0, 0, 0, 0, 2, 256, 3328, 32, false, true },
-  {0, 0, 0, 0, 3, 256, 256, 32, false, true },
-  {0, 0, 0, 0, 4, 256, 1280, 32, false, true } },
-};
+const byte numStripesPatterns = 4;
 
-/*
+stripies stripePatterns[4][4] = {
+{ {0, 0, 0, 0, 1, 256, 3584, 32, true, true },
+  {0, 0, 0, 0, 2, 256, 512, 32, true, true }, 
+  {0, 0, 0, 0, 3, 256, 1536, 32, true, true },
+  {0, 0, 0, 0, 4, 256, 2560, 32, true, true } },
+{ {0, 0, 0, 0, 1, 256, 2560, 32, false, false },
+  {0, 0, 0, 0, 2, 256, 1536, 32, false, false },
+  {0, 0, 0, 0, 3, 256, 512, 32, false, false },
+  {0, 0, 0, 0, 4, 256, 3584, 32, false, false } },
+{ {0, 0, 0, 0, 1, 256, 1536, 32, true, false },
+  {0, 0, 0, 0, 2, 256, 512, 32, true, false },
+  {0, 0, 0, 0, 3, 256, 3584, 32, true, false },
+  {0, 0, 0, 0, 4, 256, 2560, 32, true, false } },
 { {0, 0, 0, 0, 1, 256, 3328, 32, false, true },
   {0, 0, 0, 0, 2, 256, 256, 32, false, true },
   {0, 0, 0, 0, 3, 256, 1280, 32, false, true },
   {0, 0, 0, 0, 4, 256, 2304, 32, false, true } },
- */
+};
+
 
 void stripesPattern(byte pattern) {
-  for(int stripeNum = 0; stripeNum < numStripesPattern1; stripeNum++) {
+  for(int stripeNum = 0; stripeNum < numStripesPatterns; stripeNum++) {
     // I think beat stripe bassline has to start below zero 
     int stripeBeatBassline = ((timeyInTime / stripePatterns[pattern][stripeNum].speedDivisor) % 4096)-512;
     if (stripePatterns[pattern][stripeNum].forwardOrBack) {
